e855fdb53407dfaf251f62ec761880db75a44691
alwirahman <84283855+alwirahman@users.noreply.github.com>
2021-08-13 08:33:06 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Jangan Mengubah Apapun Selain Yg Didalam Tanda Kutip(")
+// Perhatikan Tanda Kutip (") Jangan Sampai Terhapus Agar Script Tidak Error
+
+$wallet = 'Masukan Wallet DOGE Disini';
+
+$user = 'Masukan User-Agent Disini';
+
+$cookie = 'Masukan Semua Cookie Disini';
+
+
+
